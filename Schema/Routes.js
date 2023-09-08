249a280cb40150cc9b64f00dea5e1dd1c4b6ca47
priyansh3731 <118509082+priyansh3731@users.noreply.github.com>
2023-09-08 08:44:36 +0530

    change

--- Schema/Routes.js
+++ Schema/Routes.js
@@ -6,7 +6,12 @@ import cors from "cors"
 const dataRoute = express.Router()
 const app = express();
 
-app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 //FUNCTION FOR CREATING DATA
 const createData = async(req,res) => {
