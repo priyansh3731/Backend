3f1e326d245d8affb1cb1f918a8159d3c1988d52
priyansh3731 <priyanshufulwari@gmail.com>
2023-09-08 11:34:44 +0530

    change

--- Schema/DataSchema.js
+++ Schema/DataSchema.js
@@ -32,15 +32,15 @@ const dataSchema = mongoose.Schema({
         required:true
     },
     photo1:{
-        type:"String",
+        type:"string",
         required:false
     },
     photo2:{
-        type:"String",
+        type:"string",
         required:false
     },
     video:{
-        type:"String",
+        type:"string",
         required:false
     }
 })
