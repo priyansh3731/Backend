f127672ec8164158a911a7392d4f18cc74ea33a4
priyansh3731 <priyanshufulwari@gmail.com>
2023-09-08 11:25:58 +0530

    change

--- Schema/Routes.js
+++ Schema/Routes.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json())
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', 'https://grumpy-jacket-lamb.cyclic.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
